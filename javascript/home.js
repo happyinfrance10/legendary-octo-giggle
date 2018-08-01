fa1ccaffff788a7adea19fc454f379704d871889
Stan Lieberman <liebermanstan@gmail.com>
2018-08-01 09:00:44 -0700

    stan: buttons

--- javascript/home.js
+++ javascript/home.js
@@ -1,11 +1,33 @@
-let box = document.querySelector(".sequence");
+// Darken each sequence box when mouse goes over it
+let sequences = document.querySelectorAll("span");
+  sequences.forEach(sequence => {
 
-  console.log(box);
-  box.addEventListener("mouseenter", e=> {
+  console.log(sequence);
+  sequence.addEventListener("mouseenter", e=> {
     console.log('mousenter', e);
-    box.classList.add("shade");
+    sequence.classList.add("shade");
   });
 
-  box.addEventListener("mouseleave", e=> {
-    box.classList.remove("shade");
+  sequence.addEventListener("mouseleave", e=> {
+    sequence.classList.remove("shade");
   });
+});
+
+
+let sequence1 = document.querySelector("#sequence1");
+sequence1.addEventListener("click", e => {
+  console.log("click");
+  window.location.href = "/level";
+});
+
+////For sequences 2 and three
+// let sequence2 = document.querySelector("#sequence2");
+// sequence1.addEventListener("click", e => {
+//   console.log("click");
+//   window.location.href = "/";
+// });
+// let sequence3 = document.querySelector("#sequence3");
+// sequence1.addEventListener("click", e => {
+//   console.log("click");
+//   window.location.href = "/";
+// });
